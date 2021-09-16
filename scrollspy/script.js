25ec5f8f8ef09da635a3c7ca23c9adcd7de44890
Alice-63 <alicetindag84@gmail.com>
2021-09-16 19:37:23 +0200

    Update script.js

--- scrollspy/script.js
+++ scrollspy/script.js
@@ -2,18 +2,20 @@
 
 const p=new Promise(function(success,error){
 
-let x=0;
+let x="Hallo";
     if(x==0)
     {
         success(x)
      
     }
     else{
-        error()
+        error(x+1)
      
     }
 })
 
 p.then(function(data){
     console.log(data)
+}).catch(function(data2){
+    console.log(data2)
 })
