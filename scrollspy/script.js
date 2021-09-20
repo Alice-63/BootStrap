7ecf2f13379337f48b0a8cc7fa4b1753ffcf2b0a
Alice-63 <alicetindag84@gmail.com>
2021-09-20 10:56:30 +0200

    Update script.js

--- scrollspy/script.js
+++ scrollspy/script.js
@@ -1,13 +1,4 @@
 
 
 
-const p=new Promise(function(success,error){
-    if(x=3)
-    {
-        error(x)
-    }
-}).then(function(){
-    console.log("Basariili")
-},function(data){
-    
-})
+console.log(this)
