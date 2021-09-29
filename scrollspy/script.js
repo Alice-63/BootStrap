0a74472ddeb6eb3b9f8139e0de4c7a8bcf244921
Alice-63 <alicetindag84@gmail.com>
2021-09-29 10:50:10 +0200

    Update script.js

--- scrollspy/script.js
+++ scrollspy/script.js
@@ -1,3 +1,19 @@
 
+async function hello(){
 
+    let response= await fetch("https://jsonplaceholder.typicode.com/todos/3");
+    let results= await response.json();
+    console.log(results)
+}
 
+hello()
+
+function hello2 (){
+    fetch("https://jsonplaceholder.typicode.com/todos/1").then(function(res){
+        return res.json()
+    }).then(function(data){
+        console.log(data)
+    })
+}
+
+hello2()
