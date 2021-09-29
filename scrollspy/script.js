83acdb98c1cd2c467845303114ace410e12e6a0e
Alice-63 <alicetindag84@gmail.com>
2021-09-29 11:27:25 +0200

    add

--- scrollspy/script.js
+++ scrollspy/script.js
@@ -1,14 +1,10 @@
 
 async function hello(){
 
-    let response= await fetch("https://jsonplaceholder.typicode.com/todos/3");
-    let results= await response.json();
+    let response= await fetch("/mona_lisa.jpg");
+    let results= await response.blob();
     console.log(results)
 }
 
 hello()
 
-function hello2 (){
-    fetch("https://jsonplaceholder.typicode.com/todos/1").then(res=> res.json()).then((data)=>{console.log(data)})}
-
-hello2()
