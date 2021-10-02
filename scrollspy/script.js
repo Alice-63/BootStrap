2070f44b4a6a7c77f31560fcf58ff2fc57f9a25f
Alice-63 <alicetindag84@gmail.com>
2021-10-02 10:12:14 +0200

    Update script.js

--- scrollspy/script.js
+++ scrollspy/script.js
@@ -10,3 +10,14 @@ async function hello(){
 }
 
 hello()
+
+function test(){
+    fetch("http://localhost.test.php")
+    .then(response => {response.json()})
+    .then(data=> console.log(data)
+  }
+  
+  async function test2(){
+    let response= await fetch("url")
+      let result= await response
+  }
